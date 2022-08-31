b674603cdc78a17116e656eece729b4a98609e38
chemsafe <chemsafe@chemicalsafety.com>
2022-08-31 13:57:06 +0300

    Initial

---
+++ ContainerApiCall/Connect.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace ContainerApiCall
+{
+    internal class Connect
+    {
+        public delegate void LogEventHandler(object sender, string message);
+
+        public event LogEventHandler MessageLogged = new LogEventHandler((e, a) => { });
+
+        public enum CallMethods
+        {
+            GetSessionId = 0,
+            AddToSession,
+            ProcessSession,
+            SessionResults,
+            GetData
+        }
+
+        public class HttpReturn
+        {
+            public string url { get; set; }
+            public string responseBody { get; set; }
+            public string errorMessage { get; set; }
+            public override string ToString()
+            {
+                return url + Environment.NewLine + responseBody + Environment.NewLine + errorMessage;
+            }
+        }
+        string _baseUrl; 
+        string _auth; 
+        string _token; 
+        public Connect(string authKey, string baseUrl,string token)
+        {
+            _token = token;
+            _auth = authKey;
+            _baseUrl = baseUrl;
+            if (!_baseUrl.EndsWith("/")) _baseUrl += "/";
+            _baseUrl = _baseUrl + "externalinterface/";
+        }
+
+        public Connect(string authKey, string baseUrl)
+        {
+            _auth = authKey;
+            _baseUrl = baseUrl;
+            if (!_baseUrl.EndsWith("/")) _baseUrl += "/";
+            _baseUrl = _baseUrl + "externalinterface/";
+        }
+        public string GetEndpoint(CallMethods action)
+        {
+            string[] endPoints = {"getsessionid", "addtosession","processsession","sessionresults","getdata"};
+            return _baseUrl + "wrappers/inventory.ashx?method=" + endPoints[(int)action];
+        }
+
+        public async Task<string> GetToken()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("auth", _auth);
+            string url = _baseUrl + "keygetter.ashx?auth=" + _auth;
+            return await client.GetStringAsync(url);
+        }
+
+        public async Task<HttpReturn> GetData(CallMethods action, string json)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("auth",_auth);
+            client.DefaultRequestHeaders.Add("key", _token);
+            var url = GetEndpoint(action);
+            if (_auth.Length == 0)
+                MessageLogged(this, "No auth set");
+            if (_token.Length == 0)
+                MessageLogged(this, "No token set");
+            MessageLogged(this, "Calling " + url);
+            if (json.Length > 0)
+                MessageLogged(this, "Post body: " + json);
+            try
+            {
+                using (var response = await client.PostAsync(url, new StringContent(json,Encoding.UTF8,"application/json")))
+                {
+                    using (var content = response.Content)
+                    {
+                        var result = await content.ReadAsStringAsync();
+                        return new HttpReturn {url=url,responseBody = result };
+                    }
+                }
+
+            }
+            catch (HttpRequestException e)
+            {
+                return new HttpReturn { url = url, errorMessage = e.Message };
+            }
+        }
+
+    }
+
+}
---
+++ ContainerApiCall/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ContainerApiCall
+{
+    internal class Program
+    {
+        static string _baseUrl = ""; //<your EMS base url here>
+        static string _authKey = ""; //<the auth key provided by Chemical Safety>
+        static string _token = "";
+
+        static  void Main(string[] args)
+        {
+            int choice = -1;
+            Console.WriteLine();
+            Console.WriteLine(@"
+  _                             __       _          
+ /  |_   _  ._ _  o  _  _. |   (_   _. _|_ _ _|_    
+ \_ | | (/_ | | | | (_ (_| |   __) (_|  | (/_ |_ \/ 
+                                                 /  
+
+");
+            while (choice != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Inventory API call example");
+                Console.WriteLine();
+                Console.WriteLine("1. GetSessionId");
+                Console.WriteLine("2. AddToSession");
+                Console.WriteLine("3. ProcessSession");
+                Console.WriteLine("4. SessionResults");
+                Console.WriteLine("5. GetData");
+                Console.WriteLine("6. GetTempToken");
+                Console.WriteLine("0. Exit");
+                string userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            GetSessionId().GetAwaiter().GetResult();
+                            break;
+                        case 2:
+                            AddContainerToSession().GetAwaiter().GetResult();
+                            break;
+                        case 3:
+                            ProcessSession().GetAwaiter().GetResult();
+                            break;
+                        case 4:
+                            SessionResults().GetAwaiter().GetResult();
+                            break;
+                        case 5:
+                            GetLookupData().GetAwaiter().GetResult();
+                            break;
+                        case 6:
+                            GetTempToken().GetAwaiter().GetResult();
+                            break;
+                    }
+                }
+            }
+        }
+
+        
+        static async Task GetTempToken()
+        {
+            var con = new Connect(_authKey, _baseUrl);
+            _token = await con.GetToken();
+            Console.WriteLine("Token received: " + _token);
+        }
+
+        private static void Con_MessageLogged(object sender, string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static async Task GetSessionId()
+        {
+            var con = new Connect(_authKey, _baseUrl,_token);
+            con.MessageLogged += Con_MessageLogged;
+            var d = new JavaScriptSerializer();
+            var result = await con.GetData(Connect.CallMethods.GetSessionId, "");
+            Console.WriteLine(result);
+        }
+
+        static async Task GetLookupData()
+        {
+            Console.WriteLine("Data to fetch: ( purchaseitem | lotnumber | facility | cas | vendor | bol | chemicalname | location | building | barcode | barcodeloc | employee | units | class | mspart )");
+            string type = Console.ReadLine();
+            var con = new Connect(_authKey, _baseUrl, _token);
+            con.MessageLogged += Con_MessageLogged;
+            var json = new PopupData
+            {
+                PopupType = type,
+                Param1 = "",
+                Param2 = "1000"
+            };
+            var d = new JavaScriptSerializer();
+            var result = await con.GetData(Connect.CallMethods.GetData, d.Serialize( json));
+            Console.WriteLine(result);
+        }
+
+
+        static async Task AddContainerToSession()
+        {
+            Console.WriteLine("Session Id");
+            string sessionId = Console.ReadLine();
+            var con = new Connect(_authKey, _baseUrl, _token);
+            con.MessageLogged += Con_MessageLogged;
+
+            var json = new ContainerUpload
+            {
+                Containers = new List<Container>() { new Container() { Barcode = "BCODE-1000",QuantityOnHand=10f,Unit="Pounds",Common="Acetone",Manufact="Sigma",ScanFlag=2} },
+                SessionId=sessionId,
+                MobileIdentifier="TEST CALLER"
+
+            };
+            var d = new JavaScriptSerializer();
+            var result = await con.GetData(Connect.CallMethods.AddToSession, d.Serialize(json));
+            Console.WriteLine(result);
+        }
+
+        static async Task ProcessSession()
+        {
+            Console.WriteLine("Session Id");
+            string sessionId = Console.ReadLine();
+            var con = new Connect(_authKey, _baseUrl, _token);
+            con.MessageLogged += Con_MessageLogged;
+
+            var json = new ProcessData
+            {
+                CustomProcessMode="default",
+                Value= sessionId
+               
+            };
+            var d = new JavaScriptSerializer();
+            var result = await con.GetData(Connect.CallMethods.ProcessSession, d.Serialize(json));
+            Console.WriteLine(result);
+        }
+
+        static async Task SessionResults()
+        {
+            Console.WriteLine("Session Id");
+            string sessionId = Console.ReadLine();
+            var con = new Connect(_authKey, _baseUrl, _token);
+            con.MessageLogged += Con_MessageLogged;
+
+            var json = new SessionResultsData
+            {
+                Value = sessionId
+
+            };
+            var d = new JavaScriptSerializer();
+            var result = await con.GetData(Connect.CallMethods.SessionResults, d.Serialize(json));
+            Console.WriteLine(result);
+        }
+
+        [Serializable]
+        class SessionResultsData
+        {
+            public string Value { get; set; }
+        }
+
+        [Serializable]
+        class ProcessData
+        {
+            public string CustomProcessMode { get; set; }
+            public string Value { get; set; }
+        }
+
+        [Serializable]
+        class PopupData
+        {
+            public string PopupType { get; set; }
+            public string Param1 { get; set; }
+            public string Param2 { get; set; }
+        }
+
+        [Serializable]
+        class Container
+        {
+            public string Barcode { get; set; }
+            public int ScanFlag { get; set; }
+            public float QuantityOnHand { get; set; }
+            public string PhysState { get; set; }
+            public string Unit { get; set; }
+            public string Common { get; set; }
+            public string Manufact { get; set; }
+            public string RecordId { get; set; }
+            public string NewBarcode { get; set; }
+            public string Number1 { get; set; }
+            public string Cas { get; set; }
+            public bool Surplus { get; set; }
+            public string PartNumber { get; set; }
+            public string OpenDate { get; set; }
+            public string LotNumber { get; set; }
+            public string PurchaseItemNumber { get; set; }
+            public string Classify { get; set; }
+            public int ItemBol { get; set; }
+        }
+
+        [Serializable]
+        class ContainerUpload
+        {
+            public List<Container> Containers { get; set; }
+            public string SessionId { get; set; }
+            public string MobileIdentifier { get; set; }
+        } 
+
+    }
+}
